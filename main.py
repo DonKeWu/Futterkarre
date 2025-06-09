c2b6d9d346b5646d7048dcaf0601c667e194643c
daniel <info@kbdk.de>
2025-06-09 09:43:07 +0200

    anlegen

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
-# main.py (aufgeräumt)
-import sys
-from PyQt5.QtWidgets import QApplication
-from views.main_window import MainWindow
-from hardware.sensor_manager import SmartSensorManager  # Statt sensor_switch
-from config.logging_config import setup_logging
-import logging
+# This is a sample Python script.
+
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def main():
-    # Logging einrichten
-    setup_logging()
-    logger = logging.getLogger(__name__)
-    logger.info("Futterkarre 2.0 startet")
-
-    app = QApplication(sys.argv)
-
-    # SensorManager statt direkter Sensor
-    sensor_manager = SmartSensorManager()
-
-    # Dummy Heu-Namen (später aus CSV laden)
-    heu_namen = ["Heu 2025", "Heulage 2025"]
-
-    window = MainWindow(sensor_manager, heu_namen)
-    window.show()
-
-    logger.info("App gestartet")
-    sys.exit(app.exec_())
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
 
 
-if __name__ == "__main__":
-    main()
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
 
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
